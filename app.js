12373b4d81d26803d7728beab35d06b776a0885a
smita patil <smitakpatil26@gmail.com>
2019-11-20 07:35:39 +0530

    updated js

---
+++ app.js
@@ -0,0 +1,22 @@
+let clientId= "033e09c0c3a6711439b5";
+let clientSecret="bbfbb86813046dee2f96a4ae87f1073b32ce0901"; 
+let userName
+function init(){
+
+console.log("SDdf")
+
+const getUser=async(user)=>{
+    const apiCall= await fetch(`https://api.github.com/users/${userName}?client_id=${clientId}&
+    client_secret=${clientSecret}`)
+    let userData=await apiCall.json();
+    return{data:userData}
+}
+$("#search").click(function(){
+    console.log("dshjdkh")
+    showData();
+})
+
+ 
+}
+
+window.onload=init;
