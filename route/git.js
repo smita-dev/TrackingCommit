57bb61a14c76ebef5391689168391b43c64ce3bb
smita patil <smitakpatil26@gmail.com>
2019-11-20 16:44:38 +0530

    getting commits

--- route/git.js
+++ route/git.js
@@ -1,5 +1,8 @@
-router.get("/username",(req,res)=>{
-    console.log("username");
+const express=require('express');
+const router=express.Router();
+router.get("/commit",(req,res)=>{
+    let commits=commitCount("https://github.com/smita-dev/Car-Becho")
+    console.log(commits)
 })
 
 module.exports=router;
