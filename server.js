936dc7cd3452639400c63488b6fc186c8264e3f0
smita patil <smitakpatil26@gmail.com>
2019-11-20 18:31:04 +0530

    added route

--- server.js
+++ server.js
@@ -13,13 +13,18 @@ app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
 
 
-const gitRoutes=require('./route/git')
-
-//filter routes
-app.use('/',gitRoutes);
-
-
 app.listen(port,()=>{
     console.log("connected on port "+port)
 })
-module.exports=app;
+
+app.get("/commit",(req,res)=>{
+    let data=req.body;
+    console.log(data)
+    let commit=commitCount("https://github.com/smita-dev/Car-Becho");
+    console.log(commit);
+    let url="https://github.com/"+req.body.username+"/"+req.body.repository;
+    let commit1=commitCount(url);
+    console.log(commit1);
+    // let commits=commitCount(`https://github.com/${req.body.username}/${req.body.repository}`)
+    // console.log(commits)
+})
