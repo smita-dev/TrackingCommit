6a52ef117608d3578a9a4a4f8feb39cb15bb1757
smita patil <smitakpatil26@gmail.com>
2019-11-20 16:44:52 +0530

    removed cors

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express=require('express');
 const app=express();
-
+const commitCount=require('git-commit-count');
 //to keep logging details
 const bodyparser=require('body-parser');
-const cors=require('cors');
+
 
 //port number
 const port=8080;
 
-//cors
-app.use(cors());
-
 //Bodyparser
 app.use(bodyparser.urlencoded({extended:false}));
 app.use(bodyparser.json());
+
 
 const gitRoutes=require('./route/git')
 
