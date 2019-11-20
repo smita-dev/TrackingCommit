675893085a4e8fcd4a958b213b67ac76e7ad6e35
smita patil <smitakpatil26@gmail.com>
2019-11-20 20:42:51 +0530

    sending commit value

--- server.js
+++ server.js
@@ -20,11 +20,10 @@ app.listen(port,()=>{
 app.get("/commit",(req,res)=>{
     let data=req.body;
     console.log(data)
-    let commit=commitCount("https://github.com/smita-dev/Car-Becho");
-    console.log(commit);
     let url="https://github.com/"+req.body.username+"/"+req.body.repository;
-    let commit1=commitCount(url);
-    console.log(commit1);
+    let commit=commitCount(url);
+    console.log(commit);
+    res.send(commit);
     // let commits=commitCount(`https://github.com/${req.body.username}/${req.body.repository}`)
     // console.log(commits)
 })
