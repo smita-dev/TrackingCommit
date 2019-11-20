d199ac74f7584a767a005c35c4b700af30c5f17e
smita patil <smitakpatil26@gmail.com>
2019-11-20 18:01:43 +0530

    deleted git.js file

--- route/git.js
+++
@@ -1,8 +0,0 @@
-const express=require('express');
-const router=express.Router();
-router.get("/commit",(req,res)=>{
-    let commits=commitCount("https://github.com/smita-dev/Car-Becho")
-    console.log(commits)
-})
-
-module.exports=router;
